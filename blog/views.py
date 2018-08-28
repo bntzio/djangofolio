cbba331836fa6199b375de4f7668b0112cf1b5a1
Enrique Benitez <enriquebenitezjr@gmail.com>
2018-08-28 02:11:01 -0500

    Polish application design

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def posts(request):
-    posts = Blog.objects
-    return render(request, 'blog/posts.html', {'posts':posts})
+    all_posts = Blog.objects
+    return render(request, 'blog/posts.html', {'posts':all_posts})
 
 def post(request, post_id):
     blog_post = get_object_or_404(Blog, pk=post_id)
